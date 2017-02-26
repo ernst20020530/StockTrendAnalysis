e07078b318183cf5af2b5cee3469139b8003f448
ernst <ernst20020530@gmail.com>
2017-02-25 21:52:48 -0500

    Feb 25

--- src/org/bwang/StockData.java
+++ src/org/bwang/StockData.java
@@ -3,8 +3,11 @@ package org.bwang;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.List;
 import java.util.OptionalDouble;
 import java.util.function.*;
+import java.util.stream.*;
+import one.util.streamex.StreamEx;
 
 import yahoofinance.Stock;
 import yahoofinance.histquotes.*;
@@ -16,8 +19,53 @@ public class StockData {
 		m_stock = s;
 	}
 	
-	OptionalDouble GetAverage(Calendar from, Calendar to, Function<HistoricalQuote, BigDecimal> getProperty) throws IOException{
-		return m_stock.getHistory(from, to).stream().map(getProperty).mapToDouble(x->x.doubleValue()).average();
+	/**
+	 * 	Get the average of one of property of HistoricalQuote
+	 * the date shall be [From, to)
+	 * param	Calendar from
+	 * param	Calendar to
+	 * param	Interval interval		DAILY, MONTHLY, WEEKLY
+	 * param	Function<HistoricalQuote, BigDecimal> getProperty	get function for close open high low price
+	 * */
+	
+	OptionalDouble GetAverage(Calendar from, Calendar to, Interval interval, Function<HistoricalQuote, BigDecimal> getProperty){
+		try {
+			return GetData(from, to, interval, getProperty).stream().mapToDouble(Double::doubleValue).average();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return OptionalDouble.empty();
+		}
+	}
+
+	/**
+	 * 	Get the list of property data from HistoricalQuote
+	 * the date shall be [From, to)
+	 * param	Calendar from
+	 * param	Calendar to
+	 * param	Interval interval		DAILY, MONTHLY, WEEKLY
+	 * param	Function<HistoricalQuote, BigDecimal> getProperty	get function for close open high low price
+	 * */
+	
+	List<Double> GetData(Calendar from, Calendar to, Interval interval, Function<HistoricalQuote, BigDecimal> getProperty) throws IOException{
+		to.add(Calendar.DATE, -1);		///move to date backward
+		return m_stock.getHistory(from, to, interval).stream().map(getProperty).mapToDouble(x->x.doubleValue()).boxed().collect(Collectors.toList());
+	}
+
+	/**
+	 * 	Get the list of difference of property data from HistoricalQuote
+	 * the date shall be [From, to)
+	 * param	Calendar from
+	 * param	Calendar to
+	 * param	Interval interval		DAILY, MONTHLY, WEEKLY
+	 * param	Function<HistoricalQuote, BigDecimal> getProperty	get function for close open high low price
+	 * */
+	
+	List<Double> GetDataDiff(Calendar from, Calendar to, Interval interval, Function<HistoricalQuote, BigDecimal> getProperty) throws IOException{
+		return StreamEx.of(GetData(from, to, interval, getProperty)).pairMap((a,b)->(Math.log(b)-Math.log(a))).collect(Collectors.toList());
+	}
+	
+	String GetSymbol(){
+		return m_symbol;
 	}
 	
 	private String m_symbol;
--- src/org/bwang/StockTrendAnalysis.java
+++ src/org/bwang/StockTrendAnalysis.java
@@ -3,8 +3,17 @@ package org.bwang;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.math.*;
 
 import yahoofinance.*;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+import org.apache.commons.math3.stat.descriptive.moment.*;
+
+import one.util.streamex.StreamEx;
 
 public class StockTrendAnalysis {
 	
@@ -12,9 +21,8 @@ public class StockTrendAnalysis {
 	
 	public Boolean initialize(){
 		try {
-			stocks = YahooFinance.get(stockSymbolList);
-			
-	
+
+			m_stockDataList = YahooFinance.get(stockSymbolList).entrySet().stream().map(x->x.getValue()).map(x->new StockData(x.getSymbol(),x)).collect(Collectors.toList());
 			
 			return true;
 		} catch (IOException e) {
@@ -22,6 +30,39 @@ public class StockTrendAnalysis {
 			return false;
 		}
 	}
+	
+	public void process(){
+		Calendar from = Calendar.getInstance();
+		from.set(2016, 12, 1);
+		Calendar to = Calendar.getInstance();
+		to.set(2017, 1, 31);
+		m_stockDataList.stream().forEach(x->System.out.println(x.GetSymbol() + " " + x.GetAverage(from, to, Interval.DAILY, HistoricalQuote::getClose).getAsDouble()));
+		
+		m_stockDataList.stream().forEach(x->{
+			List<Double> ds = null;
+			try {
+				ds = x.GetData(from, to, Interval.DAILY, HistoricalQuote::getClose);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			List<Double> closeDiff = StreamEx.of(ds).pairMap((a,b)->(Math.log(b)-Math.log(a))).collect(Collectors.toList());
+			
+		});
+		
+		
+		
+//		m_stockDataStream.filter(x->x.GetSymbol().compareTo("F") == 0).forEach(x->{
+//			try {
+//				x.GetData(from, to, HistoricalQuote::getClose).forEach(y->System.out.println(y));
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		});
+	}
+	
 	
 	static StockTrendAnalysis GetSingleton(){
 		if(singleton == null)
@@ -32,12 +73,16 @@ public class StockTrendAnalysis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StockTrendAnalysis sta = StockTrendAnalysis.GetSingleton();
+		if(!sta.initialize())
+			return;
+		
+		sta.process();
 	}
 	
 	static private StockTrendAnalysis singleton;
 	
-	static private final String[] stockSymbolList = new String[]{"XLE", "XLF"};
+	static private final String[] stockSymbolList = new String[]{"XLF", "XLE"};
 	
-	private Map<String, Stock> stocks;
+	private List<StockData> m_stockDataList;
 }
